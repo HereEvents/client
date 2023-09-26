0ac770404d300cd84c6baed23af2d4436bf9b206
Yair Ken <yair.k.ken@gmail.com>
2023-09-26 08:02:15 +0300

    replace page registeretion

---
+++ src/components/RegisterForm/index.jsx
@@ -0,0 +1,119 @@
+import styles from "./style.module.css";
+import { useState , useEffect , useContext } from "react";
+import getGoogleOAuthURL from "../../function/getGoogleOAuthURL";
+import { FcGoogle } from "react-icons/fc";
+import ClassicButton from "../ClassicButton copy";
+import Input from "../Input";
+import apiCalls from "../../function/apiCalls";
+import { setToken } from "../../function/token";
+import userContext from "../../context/userContext";
+import { useNavigate } from "react-router-dom";
+
+
+
+export default function RegisterForm({route, close}) {
+  
+  const [step , setStep] = useState(1)
+  const [isEmail,setIsEmail] = useState(false)
+
+  const {setUser} = useContext(userContext)
+
+  const navigate = useNavigate();
+
+
+  useEffect(()=>{
+    if (localStorage.lastRoute&&localStorage.Token) {
+      localStorage.removeItem('lastRoute')
+      setStep(3)
+    }
+  }
+  ,[])
+
+
+
+  async function createUser (e){
+  e.preventDefault();
+    if(step==2){
+      try {
+        const firstForm = {
+      fullName:(e.target[0].value),
+      email:   (e.target[1].value),
+    }
+      if(firstForm.email){
+        const res = await apiCalls("post", "user/creatUser",firstForm)
+        setUser(res.user);
+        setToken(res.token);
+        localStorage.setItem("Token", res.token);
+        setStep(3)
+      }
+      else{
+        console.log("no email");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  else{
+    try {
+  const fullForm = {
+    fullName:(e.target[0].value),
+    email:   (e.target[1].value),
+    phon:    (e.target[2].value),
+    city:    (e.target[3].value),
+    approval:(e.target[4].checked)
+  }
+      const res = await apiCalls("put", "user/updateDetails",fullForm)
+      setUser(res);
+  } catch (error) {
+    console.log(error);
+  }
+    close()
+  }
+    
+  }
+  return (
+    <form onSubmit={createUser} className={styles.form}>
+    <div className={styles.title}><b>{step==1?"איזה כיף שבאת!":step==2?"נעים מאוד!":"שניה לפני שנמשיך,"}</b>
+      <br /> <span className={styles.secondTitle}>{step==1?"נשמח להכיר ולעדכן על אירועים שלא בא לך לפספס!":step==2?"הרשמה קצרה וממשיכים":"השלימו את הפרופיל ותקבלו הצעות מותאמות אישית"}</span>
+    </div>
+      {(step===1)?<>
+    <a href={getGoogleOAuthURL()}>
+          <Input type="button"  noLabelAndError={true} value="אני רוצה להמשיך באמצעות      " onClick={()=>localStorage.lastRoute = route}/>
+        <div className={styles.buttonGoogle}>
+        <FcGoogle/>
+        </div>
+    </a>
+    <Input type="button" onClick={()=>setStep(2)} noLabelAndError={true} value="אני רוצה להירשם באמצעות מייל"
+    /> 
+    <div className={styles.login}>
+            יש לך חשבון?
+            <span onClick={()=>navigate("/login")} className={styles.clickHere}>
+              לחץ כאן
+            </span>
+    </div>
+      </>:
+<>
+  {step>1?<>
+    <div className={step==3?styles.hidden:null}>
+      <Input id="fullName" type="text"  name="fullName" placeholder="השם שלך" noLabelAndError={true}/>
+    </div>
+    <div className={step==3?styles.hidden:null}>
+      <Input id="email"    type="email" name="email" placeholder="המייל שלך"  noLabelAndError={true} onChange={(e)=>(setIsEmail(e.target.value!==""&&e.target.checkValidity()))}/>
+    </div>
+    <div className={step==2?styles.hidden:null}>
+      <Input id="phone"    type="tel"   name="phone" placeholder="הטלפון שלך" noLabelAndError={true}/>
+    </div>
+    <div className={step==2?styles.hidden:null}>
+      <Input id="city"     type="text"  name="city" placeholder="היישוב שלך"  noLabelAndError={true}/>
+    </div>
+      <label className={`${styles.containerCheckbox} ${step==2?styles.hidden:null}`}>
+        <input name="checkbox" type="checkbox" defaultChecked="true"/>
+        <div className={styles.labelCheckbox}> יאלה, שלחו לי אירועים שיכולים לעניין אותי!</div>
+      </label>
+  </>:null}
+    <ClassicButton width="100%" text={step==2?"יצירת חשבון":"מדהים, המשכנו!"} type="submit" disabled={step==2?!(isEmail):false}/>
+</>}
+</form>
+
+  )
+}
--- src/pages/Registeretion/index.jsx
+++ src/pages/Registeretion/index.jsx
@@ -8,163 +8,17 @@ import { IoIosCreate } from "react-icons/io";
 import apiCalls from "../../function/apiCalls";
 import { setToken } from "../../function/token";
 import userContext from "../../context/userContext";
+import RegisterForm from "../../components/RegisterForm";
 
 function Registeretion() {
+
   const { setHeader } = useContext(headerContext);
-  const { setUser } = useContext(userContext);
   setHeader("דף הרשמה");
-  const [isValid, setIsValid] = useState(true);
-  const [userData, setUserData] = useState({});
 
   const navigate = useNavigate();
 
-  const navToLoginPage = () => {
-    navigate("/login");
-  };
-
-  const inputs = [
-    {
-      id: 1,
-      name: "fullName",
-      type: "text",
-      placeholder: `🙍🏽‍♂️ שם מלא`,
-      errorMessage: "הכנס שם",
-      maxLength: "22",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "password",
-      type: "password",
-      placeholder: "🗝️ הגדר סיסמא",
-      errorMessage: "הכנס סיסמא",
-      maxLength: "14",
-      required: true,
-    },
-    {
-      id: 4,
-      name: "email",
-      type: "email",
-      placeholder: "כתובת אימייל 📧",
-      errorMessage: "הכנס אימייל תקין",
-      required: true,
-    },
-  ];
-
-  const createUser = async (e) => {
-    e.preventDefault();
-    const formElement = e.target;
-    setIsValid(formElement.checkValidity());
-    formElement.classList.add(styles.submitted);
-    const firstInvalidField = formElement.querySelector(":invalid");
-    firstInvalidField?.focus();
-    const name = userData.fullName.trim();
-    const words = name.split(" ");
-
-    // Filter out words with less than 2 letters
-    const filteredWords = words.filter((word) => word.length >= 2);
-    if (filteredWords.length >= 1) {
-        const data = userData;
-        const updatedData = {
-          ...data,
-          userType: "regular",
-        };
-        console.log(updatedData);
-      if(formElement.checkValidity()){
-        try {
-          const res = await apiCalls("post", "user/creatUser", updatedData);
-          if (!res.user.error) {
-            if (res.token) {
-              setUser(res.user);
-              setToken(res.token);
-              localStorage.setItem("Token", res.token);
-              navigate("/",{ replace: true });
-            } else {
-              alert(res);
-            }
-          } else {
-            alert(res.user.error);
-          }
-        } catch (error) {
-          alert(error);
-        }}
-    } else {
-      alert("יש לבחור שם מלא תיקני");
-    }
-  };
-
-  const handleKeyDown = (e) => {
-    const allowedKeys = /^[a-zA-Zא-ת ]$/;
-    if (
-      e.target.name === "fullName" &&
-      !allowedKeys.test(e.key) &&
-      e.key !== "Backspace"
-    ) {
-      e.preventDefault();
-    }
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === "fullName") {
-      const name = e.target.value;
-      const filteredName = name.replace(/[^a-zA-Zא-ת ]/g, "");
-      setUserData({ ...userData, [e.target.name]: filteredName });
-    } else {
-      setUserData({ ...userData, [e.target.name]: e.target.value });
-    }
-  };
-
-  // const handleChange = (e) => {
-  //   setUserData({ ...userData, [e.target.name]: e.target.value });
-  // };
-
   return (
-    <div className={styles.main}>
-      <form
-        className={styles.formArea}
-        noValidate
-        onSubmit={createUser}
-        autoComplete="on"
-      >
-        <div className={styles.header}>
-          <span> נעים מאוד :)</span>
-          <span> נשמח להכיר אותך, כמה פרטים חשובים ונמשיך</span>
-        </div>
-        <div className={styles.container}>
-          {inputs.map((input) => {
-            return (
-              <div className={styles.field}>
-                <Input
-                  isValid={isValid}
-                  key={input.id}
-                  {...input}
-                  width={"90%"}
-                  className={styles.inputs}
-                  errorMessage={input.errorMessage}
-                  onChange={handleChange}
-                  onKeyDown={handleKeyDown}
-                  maxLength={input.maxLength}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.create}>
-          <ClassicButton width={"100%"} height={"50px"} type={"submit"}>
-            <IoIosCreate className={styles.icon} /> צור משתמש
-          </ClassicButton>
-        </div>
-      </form>
-      <div className={styles.register}>
-        <div>
-          {" "}
-          יש לך כבר חשבון?
-          <span onClick={navToLoginPage} className={styles.clickHere}>
-            לחץ כאן
-          </span>
-        </div>
-      </div>
-    </div>
+<RegisterForm close={()=>navigate("/",{ replace: true })} route="/registeretion"/>
   );
 }
 
